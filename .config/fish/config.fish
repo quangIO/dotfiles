3fe8658df6a9178dacd66459f9d2e1eb9ee94b0c
Quang Luong <quangio@outlook.com>
2020-03-08 18:13:09 -0400

    fish + doom + vim

---
+++ .config/fish/config.fish
@@ -0,0 +1,14 @@
+kitty + complete setup fish | source
+
+# opam configuration
+# source /home/quangio/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+
+# ENV
+set -x ANDROID_HOME /opt/android-sdk
+set -g fish_user_paths ~/mobile/src/flutter/bin $fish_user_paths
+set -g fish_user_paths ~/.pub-cache/bin $fish_user_paths
+set -g fish_user_paths $fish_user_paths ~/.yarn/bin
+
+alias e "emacsclient -nw"
+alias l "lsd"
+alias ls "lsd"
---
+++ .doom.d/config.el
@@ -0,0 +1,77 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; refresh' after modifying this file!
+
+
+;; These are used for a number of things, particularly for GPG configuration,
+;; some email clients, file templates and snippets.
+(setq user-full-name "Luong The Minh Quang"
+      user-mail-address "luci@devel.faith")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+(setq doom-font (font-spec :family "Fira Code Medium" :size 14))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. These are the defaults.
+;; (setq doom-theme 'doom-material)
+;; (setq doom-theme 'doom-challenger-deep)
+(if (not (display-graphic-p))
+    (load-theme 'doom-material t)
+  (load-theme 'doom-challenger-deep t))
+;; If you intend to use org, it is recommended you change this!
+(setq org-directory "~/org/")
+
+;; If you want to change the style of line numbers, change this to `relative' or
+;; `nil' to disable it:
+(setq display-line-numbers-type t)
+(use-package lsp-haskell
+;;  :ensure t
+  :config
+  (setq lsp-haskell-process-path-hie "haskell-language-server")
+  (setq lsp-haskell-process-args-hie '())
+ ;; Comment/uncomment this line to see interactions between lsp client/server.
+ ;;(setq lsp-log-io t)
+)
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', where Emacs
+;;   looks when you load packages with `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; they are implemented.
+(when-let* ((dims (doom-cache-get 'last-frame-size)))
+  (cl-destructuring-bind ((left . top) width height fullscreen) dims
+    (setq initial-frame-alist
+          (append initial-frame-alist
+                  `((left . ,left)
+                    (top . ,top)
+                    (width . ,width)
+                    (height . ,height)
+                    (fullscreen . ,fullscreen))))))
+
+(defun save-frame-dimensions ()
+  (doom-cache-set 'last-frame-size
+                  (list (frame-position)
+                        (frame-width)
+                        (frame-height)
+                        (frame-parameter nil 'fullscreen))))
+
+(add-hook 'kill-emacs-hook #'save-frame-dimensions)

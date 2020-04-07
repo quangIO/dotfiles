3dc821cee1755746e68fb97d2aef5e810951bc9a
Quang Luong <quangio@outlook.com>
2020-04-07 18:36:59 -0400

    vi binding

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,13 +5,17 @@ starship init fish | source
 # opam configuration
 # source /home/quangio/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
 
-# ENV
+# Environment
 set -x ANDROID_HOME /opt/android-sdk
 set -g fish_user_paths ~/mobile/src/flutter/bin $fish_user_paths
 set -g fish_user_paths ~/.pub-cache/bin $fish_user_paths
 set -g fish_user_paths ~/.local/bin $fish_user_paths
 set -g fish_user_paths $fish_user_paths ~/.yarn/bin
 
-alias e "emacsclient -nw"
+# Bindings
+fish_vi_key_bindings default
+
+# Alias
+alias ema "primusrun emacs"
 alias l "lsd"
 alias ls "lsd"
